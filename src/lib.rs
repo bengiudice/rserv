0ce6c2c2bb07dee639101d9cbfcc2239a50d417f
benjamin giudice <ben.giudice@gmail.com>
2023-01-12 19:43:18 +0800

    Terminate workers

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::{
 };
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 struct Worker {
     id: usize,
@@ -24,19 +24,23 @@ impl ThreadPool {
         for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 }
 impl Drop for ThreadPool {
     fn drop(&mut self) {
-        for worker in  &mut self.workers {
+        drop(self.sender.take());
+        for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
             if let Some(thread) = worker.thread.take() {
                 thread.join().unwrap();
@@ -48,10 +52,21 @@ impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         // TODO: Refactor to use std::thread::Builder & spawn.
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {id} got a job; executing.");
-            job();
+            let message = receiver.lock().unwrap().recv();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
         });
-        Worker { id, thread: Some(thread), }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
