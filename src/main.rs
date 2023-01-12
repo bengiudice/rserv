515d56acc0b1ffaf79d33b9305f09212e4a542d7
benjamin giudice <ben.giudice@gmail.com>
2023-01-12 15:07:35 +0800

    Spawn 1 thread per connection

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        thread::spawn(|| handle_connection(stream));
     }
 }
 fn handle_connection(mut stream: TcpStream) {
